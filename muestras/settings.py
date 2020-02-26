2507d8eaae861eee6a67c3724322eabe05a10826
Erick <erickvasba@gmail.com>
2020-02-26 17:02:19 -0400

    no app

--- muestras/settings.py
+++ muestras/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'paginasmuestra',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
